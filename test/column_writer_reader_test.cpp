8d21de55491b6ce417e6f2081dbbe896785e934f
sjyango <sjyango2020@163.com>
2023-08-04 20:44:17 +0800

    update column writer reader test case

--- test/column_writer_reader_test.cpp
+++ test/column_writer_reader_test.cpp
@@ -17,6 +17,7 @@
 
 #include <filesystem>
 #include <random>
+#include <cmath>
 
 #include "io/file_system.h"
 #include "storage/segment_traits.h"
@@ -270,6 +271,205 @@ TEST(ColumnWriterReaderTest, StringColumnWriterReaderTest) {
 
     for (size_t i = 0; i < N; ++i) {
         ASSERT_EQ(ss[i], dst_col.get(i));
+    }
+
+    // ######################################## ColumnReader ########################################
+}
+
+TEST(ColumnWriterReaderTest, SeekInt32ColumnWriterReaderTest) {
+    const size_t N = 1000000;
+    // ######################################## ColumnWriter ########################################
+
+    io::FileWriterPtr file_writer = generate_file_writer();
+    ColumnMetaSPtr meta = std::make_shared<ColumnMeta>();
+    meta->_column_id = 0;
+    meta->_type = DataTypeFactory::instance().get_column_data_type(COLUMN_TYPE_INTEGER);
+    meta->_encoding_type = EncodingType::PLAIN_ENCODING;
+    meta->_compression_type = CompressionType::NO_COMPRESSION;
+    std::unique_ptr<ColumnWriter> column_writer = std::make_unique<ColumnWriter>(meta, file_writer.get());
+    MutableColumnSPtr dst = vectorized::ColumnFactory::instance().create_column(COLUMN_TYPE_INTEGER, "src");
+    std::vector<int32_t> src_nums;
+
+    for (size_t i = 0; i < N; ++i) {
+        src_nums.push_back(generate_random_int32());
+    }
+
+    const uint8_t* src_ptr = reinterpret_cast<const uint8_t*>(src_nums.data());
+    column_writer->append_data(&src_ptr, N);
+    column_writer->write_column_data();
+    column_writer->write_column_index();
+    file_writer->finalize();
+    file_writer->close();
+
+    // ######################################## ColumnWriter ########################################
+
+    // ######################################## ColumnReader ########################################
+
+    io::FileReaderSPtr file_reader = generate_file_reader();
+    std::unique_ptr<ColumnReader> column_reader = std::make_unique<ColumnReader>(meta, N, file_reader);
+    ordinal_t rand_ordinal = generate_random_int32() % N;
+    column_reader->seek_to_ordinal(rand_ordinal);
+    size_t read_count = 1024;
+    size_t actual_read_count = read_count;
+    column_reader->next_batch(&actual_read_count, dst);
+    ASSERT_EQ(actual_read_count, std::min(read_count, N - rand_ordinal));
+
+    ColumnInt32& dst_col = reinterpret_cast<ColumnInt32&>(*dst);
+    ASSERT_EQ(actual_read_count, dst_col.size());
+
+    for (size_t i = 0; i < actual_read_count; ++i) {
+        ASSERT_EQ(src_nums[rand_ordinal + i], dst_col.get(i));
+    }
+
+    // ######################################## ColumnReader ########################################
+}
+
+TEST(ColumnWriterReaderTest, SeekInt64ColumnWriterReaderTest) {
+    const size_t N = 1000000;
+    // ######################################## ColumnWriter ########################################
+
+    io::FileWriterPtr file_writer = generate_file_writer();
+    ColumnMetaSPtr meta = std::make_shared<ColumnMeta>();
+    meta->_column_id = 0;
+    meta->_type = DataTypeFactory::instance().get_column_data_type(COLUMN_TYPE_TIMESTAMP);
+    meta->_encoding_type = EncodingType::PLAIN_ENCODING;
+    meta->_compression_type = CompressionType::NO_COMPRESSION;
+    std::unique_ptr<ColumnWriter> column_writer = std::make_unique<ColumnWriter>(meta, file_writer.get());
+    MutableColumnSPtr dst = vectorized::ColumnFactory::instance().create_column(COLUMN_TYPE_TIMESTAMP, "src");
+    std::vector<int64_t> src_nums;
+
+    for (size_t i = 0; i < N; ++i) {
+        src_nums.push_back(generate_random_timestamp());
+    }
+
+    const uint8_t* src_ptr = reinterpret_cast<const uint8_t*>(src_nums.data());
+    column_writer->append_data(&src_ptr, N);
+    column_writer->write_column_data();
+    column_writer->write_column_index();
+    file_writer->finalize();
+    file_writer->close();
+
+    // ######################################## ColumnWriter ########################################
+
+    // ######################################## ColumnReader ########################################
+
+    io::FileReaderSPtr file_reader = generate_file_reader();
+    std::unique_ptr<ColumnReader> column_reader = std::make_unique<ColumnReader>(meta, N, file_reader);
+    ordinal_t rand_ordinal = generate_random_int32() % N;
+    column_reader->seek_to_ordinal(rand_ordinal);
+    size_t read_count = 1024;
+    size_t actual_read_count = read_count;
+    column_reader->next_batch(&actual_read_count, dst);
+    ASSERT_EQ(actual_read_count, std::min(read_count, N - rand_ordinal));
+
+    ColumnInt64& dst_col = reinterpret_cast<ColumnInt64&>(*dst);
+    ASSERT_EQ(actual_read_count, dst_col.size());
+
+    for (size_t i = 0; i < actual_read_count; ++i) {
+        ASSERT_EQ(src_nums[rand_ordinal + i], dst_col.get(i));
+    }
+
+    // ######################################## ColumnReader ########################################
+}
+
+TEST(ColumnWriterReaderTest, SeekFloat64ColumnWriterReaderTest) {
+    const size_t N = 1000000;
+    // ######################################## ColumnWriter ########################################
+
+    io::FileWriterPtr file_writer = generate_file_writer();
+    ColumnMetaSPtr meta = std::make_shared<ColumnMeta>();
+    meta->_column_id = 0;
+    meta->_type = DataTypeFactory::instance().get_column_data_type(COLUMN_TYPE_DOUBLE_FLOAT);
+    meta->_encoding_type = EncodingType::PLAIN_ENCODING;
+    meta->_compression_type = CompressionType::NO_COMPRESSION;
+    std::unique_ptr<ColumnWriter> column_writer = std::make_unique<ColumnWriter>(meta, file_writer.get());
+    MutableColumnSPtr dst = vectorized::ColumnFactory::instance().create_column(COLUMN_TYPE_DOUBLE_FLOAT, "src");
+    std::vector<double_t> src_nums;
+
+    for (size_t i = 0; i < N; ++i) {
+        src_nums.push_back(generate_random_float64());
+    }
+
+    const uint8_t* src_ptr = reinterpret_cast<const uint8_t*>(src_nums.data());
+    column_writer->append_data(&src_ptr, N);
+    column_writer->write_column_data();
+    column_writer->write_column_index();
+    file_writer->finalize();
+    file_writer->close();
+
+    // ######################################## ColumnWriter ########################################
+
+    // ######################################## ColumnReader ########################################
+
+    io::FileReaderSPtr file_reader = generate_file_reader();
+    std::unique_ptr<ColumnReader> column_reader = std::make_unique<ColumnReader>(meta, N, file_reader);
+    ordinal_t rand_ordinal = generate_random_int32() % N;
+    column_reader->seek_to_ordinal(rand_ordinal);
+    size_t read_count = 1024;
+    size_t actual_read_count = read_count;
+    column_reader->next_batch(&actual_read_count, dst);
+    ASSERT_EQ(actual_read_count, std::min(read_count, N - rand_ordinal));
+
+    ColumnFloat64& dst_col = reinterpret_cast<ColumnFloat64&>(*dst);
+    ASSERT_EQ(actual_read_count, dst_col.size());
+
+    for (size_t i = 0; i < actual_read_count; ++i) {
+        ASSERT_EQ(src_nums[rand_ordinal + i], dst_col.get(i));
+    }
+
+    // ######################################## ColumnReader ########################################
+}
+
+TEST(ColumnWriterReaderTest, SeekStringColumnWriterReaderTest) {
+    const size_t N = 100000;
+    // ######################################## ColumnWriter ########################################
+
+    io::FileWriterPtr file_writer = generate_file_writer();
+    ColumnMetaSPtr meta = std::make_shared<ColumnMeta>();
+    meta->_column_id = 0;
+    meta->_type = DataTypeFactory::instance().get_column_data_type(COLUMN_TYPE_STRING);
+    meta->_encoding_type = EncodingType::PLAIN_ENCODING;
+    meta->_compression_type = CompressionType::NO_COMPRESSION;
+    std::unique_ptr<ColumnWriter> column_writer = std::make_unique<ColumnWriter>(meta, file_writer.get());
+    MutableColumnSPtr dst = vectorized::ColumnFactory::instance().create_column(COLUMN_TYPE_STRING, "src");
+    std::vector<std::string> ss;
+    std::vector<Slice> slices;
+
+    for (size_t i = 0; i < N; ++i) {
+        ss.push_back(generate_random_string(generate_random_int32() % 1000));
+
+    }
+
+    for (size_t i = 0; i < N; ++i) {
+        slices.push_back(ss[i]);
+        ASSERT_EQ(ss[i], slices[i].to_string());
+    }
+
+    const uint8_t* src_ptr = reinterpret_cast<const uint8_t*>(slices.data());
+    column_writer->append_data(&src_ptr, N);
+    column_writer->write_column_data();
+    column_writer->write_column_index();
+    file_writer->finalize();
+    file_writer->close();
+
+    // ######################################## ColumnWriter ########################################
+
+    // ######################################## ColumnReader ########################################
+
+    io::FileReaderSPtr file_reader = generate_file_reader();
+    std::unique_ptr<ColumnReader> column_reader = std::make_unique<ColumnReader>(meta, N, file_reader);
+    ordinal_t rand_ordinal = generate_random_int32() % N;
+    column_reader->seek_to_ordinal(rand_ordinal);
+    size_t read_count = 1024;
+    size_t actual_read_count = read_count;
+    column_reader->next_batch(&actual_read_count, dst);
+    ASSERT_EQ(actual_read_count, std::min(read_count, N - rand_ordinal));
+
+    ColumnString& dst_col = reinterpret_cast<ColumnString&>(*dst);
+    ASSERT_EQ(actual_read_count, dst_col.size());
+
+    for (size_t i = 0; i < actual_read_count; ++i) {
+        ASSERT_EQ(ss[rand_ordinal + i], dst_col.get(i));
     }
 
     // ######################################## ColumnReader ########################################
