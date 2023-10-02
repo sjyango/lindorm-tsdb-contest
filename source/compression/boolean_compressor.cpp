d9b570c310e77b4675f743519b1cfc615d2db519
Alec Henx <alec.he@vesoft.com>
2023-10-02 11:38:16 +0800

    int compress

---
+++ source/compression/boolean_compressor.cpp
@@ -0,0 +1,11 @@
+#include "compression/boolean_compression.h"
+#include "../source/compression/bitpack/include/bitpacker/bitpacker.hpp"
+
+namespace LindormContest::compression {
+uint32_t CompressionBitPack::compress(const char* source, uint32_t source_size, char* dest) const {
+    return -1;
+}
+uint8_t CompressionBitPack::decompress(const char* source, uint32_t source_size, char* dest, uint32_t uncompressed_size) const {
+    return -1;
+}
+}
